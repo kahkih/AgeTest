949b92757fb2bd4e5ca267ebd10208f099a0edc5
Cake <38938390+kahkih@users.noreply.github.com>
2018-09-03 09:30:24 +0200

    Add files via upload

---
+++ Age_Test/Age_Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //writing test program to check user feedback input and output.
+            Console.Write("What is your age?");
+            
+            //I want to have the user input and use it to compare.
+            string ageUserInputString = Console.ReadLine();
+
+            int ageUser = int.Parse(ageUserInputString);
+
+            int ageKatyPerry = 33;
+
+            if(ageUser > 33)
+            {
+                Console.Write("You are younger than Katy Perry");
+            }
+            else if (ageUser == ageKatyPerry)
+            {
+                Console.Write("You are the same age as Katy Perry");
+            }
+            else if (ageUser < 0)
+            {
+                Console.Write("Try again...");
+             
+            }
+            else
+            {
+                Console.Write
+                    ("You are younger than Katy Perry.");
+            }
+            Console.ReadLine();  
+        }
+    }
+}
